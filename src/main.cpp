7972ede35a5668725e626f9ff95587d0b8536657
Luki446 <luki446@gmail.com>
2018-09-09 23:56:02 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#include <cstdio>
+#include <cstdlib>
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+const GLuint WIDTH = 860, HEIGHT = 640;
+
+int main(int argc, char* argv[])
+{
+    if(glfwInit() == 0)
+    {
+        fprintf(stderr, "GLFW initialization error");
+        return EXIT_FAILURE;
+    }
+
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Sandbox", NULL, NULL);
+
+    if(window == NULL)
+    {
+        fprintf(stderr, "Window initialization error");
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+
+    float ratio;
+
+    while(!glfwWindowShouldClose(window))
+    {
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        {
+            glfwDestroyWindow(window);
+        }
+
+        glViewport(0, 0, WIDTH, HEIGHT);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+        ratio = WIDTH / (float) HEIGHT;
+
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
